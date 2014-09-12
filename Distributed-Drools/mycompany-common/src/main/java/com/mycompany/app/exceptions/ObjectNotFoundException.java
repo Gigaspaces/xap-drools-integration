2c5ca179f5a20501ff464de3faa1e9b6d7135aad
Allen Terleto <allen@gigaspaces.com>
2014-09-12 13:16:45 -0400

    Initial version of Distributed-Drools project

---
+++ Distributed-Drools/mycompany-common/src/main/java/com/mycompany/app/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.mycompany.app.exceptions;
+
+public class ObjectNotFoundException extends Exception {
+
+    private static final long serialVersionUID = 6249584546471813739L;
+
+	public ObjectNotFoundException(String s) {
+        super(s);
+    }
+	
+}
